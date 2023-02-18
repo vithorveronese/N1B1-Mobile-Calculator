0ad67979aa47b336fea2a5d1bb8ec3f938cf4f8f
Vithor Llaguno Veronese <vithor.veronese@outlook.com>
2023-02-18 13:22:06 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import styles from './styles'
 
 export default function App() {
+
+  const [valor1, setValor1] = useState(0);
+  const [valor2, setValor2] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  function soma() {
+    let r = Number.parseFloat(valor1.replace(',', '.')) +
+                    Number.parseFloat(valor2.replace(',', '.'));
+                    setResultado(r);
+                    Keyboard.dismiss();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Calculadora</Text>
       <StatusBar style="auto" />
+      <Text style={styles.numbersBox}>Caixa de números</Text>
+      <View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.numberBox}>+</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.numberBox}>-</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+          <Text style={styles.numberBox}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>^</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>/</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.numberBox}>=</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+      <TextInput keyboardType='decimal-pad'  onChangeText={(text)=>setValor1(text)}>
+      </TextInput>
+
+      <TouchableOpacity onPress={()=> soma()}>
+
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,44 @@
+import { StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    numbersBox: {
+      borderColor: "#456",
+      borderWidth: 2,
+      width: "70%",
+      height: "5%",
+      marginBottom: "4%",
+      marginTop: "5%",
+    },
+    calcButtonsContainer: {
+      flexDirection: 'row',
+    },
+    numberRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-around'
+    },
+    numberBox: {
+      borderColor: "#000",
+      borderWidth: 2,
+      alignItems: 'center',
+      width: 55,
+      height: 55,
+      textAlign: 'center',
+      fontSize: 40,
+    },
+    pressableOperationsBox: {
+      borderColor: "#fde",
+      borderWidth: 5,
+      alignItems: 'center',
+      width: "30%",
+      height: "45%",
+    },
+
+});
+
+export default styles;
